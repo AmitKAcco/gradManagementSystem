e11440b8055715c4f8ba50c5e4acc39d98257981
prina2111 <prina.gudhka@accolitedigital.com>
2022-12-30 17:25:59 +0530

    Changes to be committed: 	modified:   src/app/graduate/feedback/view-feedback/view-feedback.component.html 	modified:   src/app/graduate/feedback/view-feedback/view-feedback.component.ts
    made reusable table for feedback

--- src/app/graduate/feedback/view-feedback/view-feedback.component.ts
+++ src/app/graduate/feedback/view-feedback/view-feedback.component.ts
@@ -11,10 +11,23 @@ import { FeedbackService } from '../feedback.service';
 })
 export class ViewFeedbackComponent {
   tasks: Task[] = [];
-  constructor(private feedback: FeedbackService) {
+  constructor(private feedback: FeedbackService) {}
 
-  }
+  dataSource:any;
+
   ngOnInit(): void {
-    this.feedback.positonBehaviourA.subscribe(d => this.tasks.push(d));
+    this.feedback.positonBehaviourA.subscribe(data => {
+      this.tasks.push(data);
+      this.dataSource = [data];
+    });
   }
+
+  columns = [
+   
+    { columnDef: 'id', header: 'Id',    cell: (element: any) => `${element.id}` },
+    { columnDef: 'title',     header: 'Title',   cell: (element: any) => `${element.title}`     },
+    { columnDef: 'note',   header: 'Note', cell: (element: any) => `${element.note}`   },
+ 
+  ];
+
 }
