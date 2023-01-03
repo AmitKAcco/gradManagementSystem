c797da9800137ce4546193d0f667a703bb4cfb6b
Amit Kumar <amit.kumar@accolitedigital.com>
2023-01-03 21:55:12 +0530

    Changes to be committed: 	modified:   src/app/organizer/approval-lead/calender-approval/calender-approval.module.ts 	modified:   src/app/organizer/approval-lead/calender-approval/home/home.component.html 	modified:   src/app/organizer/approval-lead/calender-approval/home/home.component.ts 	modified:   src/app/organizer/technical-manager/add-project/add-project.module.ts
    Changes not staged for commit:
    	modified:   src/app/organizer/approval-lead/childroute.module.ts
    	deleted:    src/app/organizer/approval-lead/graduate-details/graduate-details-routing.module.ts
    	deleted:    src/app/organizer/approval-lead/graduate-details/graduate-details.module.ts
    	deleted:    src/app/organizer/approval-lead/graduate-details/home/home.component.html
    	deleted:    src/app/organizer/approval-lead/graduate-details/home/home.component.scss
    	deleted:    src/app/organizer/approval-lead/graduate-details/home/home.component.spec.ts
    	deleted:    src/app/organizer/approval-lead/graduate-details/home/home.component.ts
    	modified:   src/app/organizer/graduate-manager/childroute.module.ts
    	deleted:    src/app/organizer/graduate-manager/graduate-details/graduate-details-routing.module.ts
    	deleted:    src/app/organizer/graduate-manager/graduate-details/graduate-details.module.ts
    	deleted:    src/app/organizer/graduate-manager/graduate-details/home/home.component.html
    	deleted:    src/app/organizer/graduate-manager/graduate-details/home/home.component.scss
    	deleted:    src/app/organizer/graduate-manager/graduate-details/home/home.component.spec.ts
    	deleted:    src/app/organizer/graduate-manager/graduate-details/home/home.component.ts
    	modified:   src/app/organizer/others/childroute.module.ts
    	modified:   src/app/organizer/others/curriculum-view/curriculum-view.module.ts
    	modified:   src/app/organizer/others/curriculum-view/home/home.component.html
    	deleted:    src/app/organizer/others/graduate-details/graduate-details-routing.module.ts
    	deleted:    src/app/organizer/others/graduate-details/graduate-details.module.ts
    	deleted:    src/app/organizer/others/graduate-details/home/home.component.html
    	deleted:    src/app/organizer/others/graduate-details/home/home.component.scss
    	deleted:    src/app/organizer/others/graduate-details/home/home.component.spec.ts
    	deleted:    src/app/organizer/others/graduate-details/home/home.component.ts
    	modified:   src/app/organizer/technical-delivery/curriculum-design/curriculum-design.module.ts
    
    Untracked files:
    	src/app/organizer/approval-lead/graduate-detail-app-lead/
    	src/app/organizer/graduate-manager/graduate-detail-grad-mang/
    	src/app/organizer/others/graduate-details-others/

--- src/app/organizer/approval-lead/calender-approval/home/home.component.ts
+++ src/app/organizer/approval-lead/calender-approval/home/home.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  batchId = [0, 1, 2];
-  currVal = 0;
-  changed(){
-    this.currVal 
+  addProject : FormGroup;
+  batches = [1,2,3];
+  selectedBatch = 1;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.addProject = this.fb.group({
+          projectName:[''],
+          batch:['']
+      })
   }
+
+  onChangeValue(newValue: any) {
+    console.log(newValue);
+    this.selectedBatch = newValue;
+  }
+  
+  onSubmit(){
+    console.log(this.addProject.value);
+  }
+    
 }
