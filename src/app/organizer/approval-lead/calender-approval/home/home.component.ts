e75afb29d9e6f570f3d4f9089338b06af26769e0
Amit Kumar <amit.kumar@accolitedigital.com>
2023-01-04 02:28:48 +0530

    Changes to be committed: 	modified:   src/app/global.service.ts 	modified:   src/app/organizer/approval-lead/calender-approval/calender-approval.module.ts 	modified:   src/app/organizer/approval-lead/calender-approval/home/home.component.html 	modified:   src/app/organizer/approval-lead/calender-approval/home/home.component.ts 	modified:   src/app/organizer/approval-lead/curriculum-approval/curriculum-approval.module.ts 	modified:   src/app/organizer/approval-lead/curriculum-approval/home/home.component.html 	modified:   src/app/organizer/approval-lead/curriculum-approval/home/home.component.ts 	modified:   src/app/organizer/technical-manager/add-project/add/add.component.html 	modified:   src/app/organizer/technical-manager/add-project/add/add.component.ts 	new file:   src/app/organizer/technical-manager/add-project/add/batchData.ts 	new file:   src/app/organizer/technical-manager/add-project/addproject-service.service.spec.ts 	new file:   src/app/organizer/technical-manager/add-project/addproject-service.service.ts

--- src/app/organizer/approval-lead/calender-approval/home/home.component.ts
+++ src/app/organizer/approval-lead/calender-approval/home/home.component.ts
@@ -7,25 +7,6 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  addProject : FormGroup;
-  batches = [1,2,3];
-  selectedBatch = 1;
-  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-    this.addProject = this.fb.group({
-          projectName:[''],
-          batch:['']
-      })
-  }
 
-  onChangeValue(newValue: any) {
-    console.log(newValue);
-    this.selectedBatch = newValue;
-  }
-  
-  onSubmit(){
-    console.log(this.addProject.value);
-  }
-    
 }
--- src/app/organizer/approval-lead/curriculum-approval/home/home.component.ts
+++ src/app/organizer/approval-lead/curriculum-approval/home/home.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { GlobalService } from 'src/app/global.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  batches = [1, 2, 3];
+  selectedBatch = 2;
+  data: any;
+  constructor(private approvalService: GlobalService) {
+
+  }
+  onChangeValue(newValue: any) {
+    console.log(newValue);
+    this.selectedBatch = newValue;
+  }
+  sendApproval() {
+    this.data = {
+      batchId: this.selectedBatch,
+      techCurriculum: true
+    }
+    this.approvalService.postTopic(this.data)
+      .subscribe(
+
+    );
+  }
+  sendDisApproval() {
+
+  }
 }
