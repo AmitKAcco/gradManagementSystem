eeed9bddaf00017da2388f97b6be205f9f6abf21
ridhi-accolite22 <ridhi.kar@accolitedigital.com>
2023-01-03 13:09:29 +0530

    worked on project calendar and mock interview forms Changes to be committed: 	modified:   src/app/organizer/technical-manager/mock-interview/add-mock-interview/add-mock-interview.component.html 	modified:   src/app/organizer/technical-manager/mock-interview/add-mock-interview/add-mock-interview.component.ts 	modified:   src/app/organizer/technical-manager/mock-interview/home/home.component.html 	modified:   src/app/organizer/technical-manager/mock-interview/home/home.component.ts 	modified:   src/app/organizer/technical-manager/mock-interview/mock-interview-routing.module.ts 	modified:   src/app/organizer/technical-manager/project-calendar/add/add.component.html 	modified:   src/app/organizer/technical-manager/project-calendar/add/add.component.ts 	modified:   src/app/organizer/technical-manager/project-calendar/home/home.component.html 	modified:   src/app/organizer/technical-manager/project-calendar/home/home.component.ts 	modified:   src/app/organizer/technical-manager/project-calendar/project-calendar-routing.module.ts 	modified:   src/app/organizer/technical-manager/project-calendar/project-calendar.module.ts

--- src/app/organizer/technical-manager/project-calendar/add/add.component.ts
+++ src/app/organizer/technical-manager/project-calendar/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent {
+
+  projectCalendar : FormGroup;
+  constructor(private fb: FormBuilder) { }
+  batchIdList = [];
+  ngOnInit() {
+    this.projectCalendar = this.fb.group({
+       teamNumber : [''],
+       projectName : [''],
+       evaluationDate : Date,
+       projectProgress : [''],
+       description : [''],
+       selectBatchId : ['']
+    })
+  }
+ 
+  onSubmit(){
+    console.log(this.projectCalendar.value);
+  }
 
 }
--- src/app/organizer/technical-manager/project-calendar/home/home.component.ts
+++ src/app/organizer/technical-manager/project-calendar/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  navLinks: any[];
+  activeLinkIndex = -1;
 
+
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Add Project Calendar',
+        link: './add',
+        index: 0
+      }, 
+      {
+        label: 'View Project Calendar',
+        link: './view',
+        index: 1
+      }
+    ];
+
+}  ngOnInit() {
+  this.router.events.subscribe((res) => {
+    this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+  });
+}
 }
