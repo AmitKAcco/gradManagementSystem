8c9490fef35a48b83258d14c22ac415bdf36dbe3
4119twinkle <twinkle.das@accolitedigital.com>
2023-01-04 11:24:12 +0530

    Worked on technical-calendar, integrated with backend

--- src/app/organizer/technical-manager/technical-clalender/technical-clalender-routing.module.ts
+++ src/app/organizer/technical-manager/technical-clalender/technical-clalender-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCalendarComponent } from './add-calendar/add-calendar.component';
 import { HomeComponent } from './home/home.component';
+import { ViewCalendarComponent } from './view-calendar/view-calendar.component';
 
 const routes: Routes = [
   {
     path : '',
-    component : HomeComponent
+    component : HomeComponent,
+    children : [
+      {
+        path: '',
+        redirectTo:'add',
+        pathMatch: 'full'
+      },
+      {
+        path: 'add',
+        component: AddCalendarComponent
+      },
+      {
+        path: 'view',
+        component: ViewCalendarComponent
+      }
+    ]
   }
 ];
 
--- src/app/organizer/technical-manager/technical-clalender/technical-clalender.module.ts
+++ src/app/organizer/technical-manager/technical-clalender/technical-clalender.module.ts
@@ -3,15 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { TechnicalClalenderRoutingModule } from './technical-clalender-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AddCalendarComponent } from './add-calendar/add-calendar.component';
+import { ViewCalendarComponent } from './view-calendar/view-calendar.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { WidgetsModule } from 'src/app/shared/widgets/widgets.module';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AddCalendarComponent,
+    ViewCalendarComponent
   ],
   imports: [
     CommonModule,
-    TechnicalClalenderRoutingModule
+    TechnicalClalenderRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatTabsModule,
+    WidgetsModule
   ]
 })
 export class TechnicalClalenderModule { }
