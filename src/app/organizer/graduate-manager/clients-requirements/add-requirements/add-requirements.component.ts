d3b1a8803f681026ac9a28246883599434bcc0e9
ridhi-accolite22 <ridhi.kar@accolitedigital.com>
2023-01-04 11:20:50 +0530

    added client requirements form with add and view components and integration

--- src/app/organizer/graduate-manager/clients-requirements/add-requirements/add-requirements.component.ts
+++ src/app/organizer/graduate-manager/clients-requirements/add-requirements/add-requirements.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectorRef, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { ClientrequirementsService } from '../clientrequirements.service';
 @Component({
   selector: 'app-add-requirements',
   templateUrl: './add-requirements.component.html',
   styleUrls: ['./add-requirements.component.scss']
 })
 export class AddRequirementsComponent {
+
+  clientRequirements : FormGroup;
+  constructor(private fb: FormBuilder ,private clientRequirementService : ClientrequirementsService) { }
+  jobIdList = [];
+  batchIdList = [];
+
+  ngOnInit() {
+    this.clientRequirements = this.fb.group({
+       selectBatchId : [''],
+       selectJobId : [''],
+       noOfPositions : [''],
+       stakeholder : [''],
+       department : [''],
+       location : [''],
+       gender : ['']
+      
+       
+    })
+  }
+ 
+  onSubmit(){
+    console.log(this.clientRequirements.value);
+    this.clientRequirementService.postClientRequirements(this.clientRequirements.value)
+    .subscribe(
+      // response => console.log('Success!', response),
+      // error => console.error('Error!', error)
+    );
+  }
 
 }
--- src/app/organizer/graduate-manager/clients-requirements/home/home.component.ts
+++ src/app/organizer/graduate-manager/clients-requirements/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  navLinks: any[];
+  activeLinkIndex = -1;
+
+
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Add Client Requirement',
+        link: './add',
+        index: 0
+      }, 
+      {
+        label: 'View Client Requirement',
+        link: './view',
+        index: 1
+      }
+    ];
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
 
 }
