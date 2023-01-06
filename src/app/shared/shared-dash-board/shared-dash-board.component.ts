5e44304f8936619700db70d90b8d21b403439c74
Amit Kumar <amit.kumar@accolitedigital.com>
2023-01-06 13:07:58 +0530

    modified:   src/app/shared/shared-dash-board/shared-dash-board.component.ts
    Changes not staged for commit:
    	modified:   src/app/shared/shared-dash-board/shared-dash-board.component.html
    	modified:   src/app/shared/shared-dash-board/shared-dash-board.service.ts
    	modified:   src/app/shared/shared.module.ts

--- src/app/shared/shared-dash-board/shared-dash-board.component.ts
+++ src/app/shared/shared-dash-board/shared-dash-board.component.ts
@@ -1,4 +1,10 @@
 import { Component , OnInit, Input} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable, of } from 'rxjs';
+import { GlobalService } from 'src/app/global.service';
+import { Task } from 'src/app/graduate/feedback/shared/models/task.model';
+import { batchesGet } from 'src/backend.Data';
+import { SharedDashBoardService } from './shared-dash-board.service';
 
 @Component({
   selector: 'app-shared-dash-board',
@@ -6,5 +12,43 @@ import { Component , OnInit, Input} from '@angular/core';
   styleUrls: ['./shared-dash-board.component.scss']
 })
 export class SharedDashBoardComponent {
+
+  batches :any;
+  storeBatchName : batchesGet[];
+  show : boolean = false;
+  totalGrads : number = 0;
+  selectedGrads : number = 0;
+  totalReq : number = 0;
+  unplacedGrads : number = 0;
+  constructor(private sharedDashSer : SharedDashBoardService,private global : GlobalService){
+  }
+  tasks : Task[] = [];
+  ngOnInit() {
+    this.global.getAllBatches().subscribe(data=>{
+      this.storeBatchName = data;
+    })
+   
+  }
+ 
+
+  filterData(num: any, num1 :any){
+    console.log(num);
+    console.log(num1)
+    this.sharedDashSer.getcount(num1).subscribe(data=>{
+      this.totalGrads = data;
+    });
+    this.sharedDashSer.getcountselected(num1).subscribe(data=>{
+      console.log(data);
+      this.selectedGrads = data; 
+    })
+    this.sharedDashSer.getcountRequstion(num1).subscribe(data=>{
+      this.totalReq = data;
+    })
+    setTimeout(() => {
+      this.unplacedGrads = this.totalGrads - this.selectedGrads;
+      this.show = true;
+    }, 500);
+  }
+
 
 }
